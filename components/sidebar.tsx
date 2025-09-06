e19700192e676709d1521661012ffc1d0aba8197
Fahim Ahmed Emon <63098250+fahim37@users.noreply.github.com>
2025-09-06 09:20:19 -0700

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -111,11 +111,3 @@ export function Sidebar() {
   )
 }
 
-
-echo "# bfoll-user-dash" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/fahim37/bfoll-user-dash.git
-git push -u origin main
